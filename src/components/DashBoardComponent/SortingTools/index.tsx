0b09ef6d2625781efcef32d53c7aacc56c656c6b
Maxim <panda.gtr007@gmail.com>
2023-05-03 18:18:34 +0600

    add TODO

--- src/components/DashBoardComponent/SortingTools/index.tsx
+++ src/components/DashBoardComponent/SortingTools/index.tsx
@@ -5,6 +5,8 @@ import style from './SortingTools.module.scss';
 export const SortingTools: React.FC = () => {
   return (
     <div className={style.sort}>
+      {/* //TODO: сделать подгрузку с backend следующих карточек */}
+      {/* //TODO: сделать состояние выбранного фильтра */}
       <button type="button" data-filter="all">
         Все
       </button>
@@ -14,14 +16,14 @@ export const SortingTools: React.FC = () => {
       <button type="button" data-filter=".email">
         Email
       </button>
-      <div className={style.order}>
+      {/* <div className={style.order}>
         <button type="button" data-sort="order:asc">
           Новые
         </button>
         <button type="button" data-sort="order:descending">
           Старые
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
